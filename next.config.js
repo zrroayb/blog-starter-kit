2ce97a383d43a4127bd18019098bde496a06a99a
zrroayb <yamadera1m@gmail.com>
2025-02-08 13:02:17 +0300

    vecrcel için türkçe değişiklik kaldırma

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // App Router ile i18n yapılandırması farklı şekilde yapılıyor
-  // i18n konfigürasyonunu kaldırıyoruz
-};
-
-module.exports = nextConfig;
