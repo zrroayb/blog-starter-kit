370fa17e1cfb7d87baa112fc7cb38707610b4418
zrroayb <yamadera1m@gmail.com>
2025-02-10 00:55:57 +0300

    Add individual blog post pages: - Add dynamic route for blog posts - Add post detail page - Update post preview links - Match website UI - Add metadata for blog posts

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -27,7 +27,11 @@ export function PostPreview({
         <CoverImage slug={slug} title={title} src={coverImage} />
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link href={`/posts/${slug}`} className="hover:underline">
+        <Link
+          as={`/posts/${slug}`}
+          href="/posts/[slug]"
+          className="hover:underline"
+        >
           {title}
         </Link>
       </h3>
